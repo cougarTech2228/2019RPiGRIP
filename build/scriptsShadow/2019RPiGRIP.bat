56adb62df9ff2fee99c2a6a36d16d54b67e43f85
rsymons22 <sssburgershack@gmail.com>
2019-02-28 18:49:58 -0500

    Updated Vision Code, piGripContours -Brenden and Ryan

--- build/scriptsShadow/2019RPiGRIP.bat
+++ build/scriptsShadow/2019RPiGRIP.bat
@@ -1,7 +1,7 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
-@rem  RPiGrip startup script for Windows
+@rem  2019RPiGRIP startup script for Windows
 @rem
 @rem ##########################################################################
 
@@ -13,7 +13,7 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%..
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and R_PI_GRIP_OPTS to pass JVM options to this script.
+@rem Add default JVM options here. You can also use JAVA_OPTS and 2019_R_PI_GRIP_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
@@ -63,19 +63,19 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\RPiGrip-all.jar
+set CLASSPATH=%APP_HOME%\lib\2019RPiGRIP-all.jar
 
-@rem Execute RPiGrip
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %R_PI_GRIP_OPTS%  -jar "%CLASSPATH%" %CMD_LINE_ARGS%
+@rem Execute 2019RPiGRIP
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %2019_R_PI_GRIP_OPTS%  -jar "%CLASSPATH%" %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
-rem Set variable R_PI_GRIP_EXIT_CONSOLE if you need the _script_ return code instead of
+rem Set variable 2019_R_PI_GRIP_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-if  not "" == "%R_PI_GRIP_EXIT_CONSOLE%" exit 1
+if  not "" == "%2019_R_PI_GRIP_EXIT_CONSOLE%" exit 1
 exit /b 1
 
 :mainEnd
